8f1cd84ebbc2b89d9325d53246e9b5c7c167915d
MegMcC <116989994+Meg-Hinckley35@users.noreply.github.com>
2023-05-04 16:10:03 -0400

    started manager test

---
+++ test/manager.test.js
@@ -0,0 +1,47 @@
+const Manager = require("../lib/manager");
+
+describe("Manager", () => {
+    const mockIntern = {
+        name: "Amy",
+        id: 23,
+        email: "amya@test.com",
+        officeNumber: 305,
+    };
+    describe("constructor tests", () => {
+        test("should construct a new instance of the intern class", () => {
+          const intern = new Intern(mockIntern);
+          expect(intern).toBeInstanceOf(Intern);
+        });
+        test("should construct a new instance of Intern class with name, id, email, and school", () => {
+            const intern = new Intern(mockIntern);
+            expect(intern).toEqual({
+                name: "Jessica",
+                id: 170,
+                email: "jessica@test.com",
+                school: "Columbia University",
+            });
+        });
+    });
+    describe("method tests", () => {
+        test("should return id when getId is called", () => {
+            const intern = new Intern(mockIntern);
+            expect(intern.getId()).toEqual(170);
+        });
+        test("should return name when getName is called", () => {
+            const intern = new Intern(mockIntern);
+            expect(intern.getName()).toEqual("Jessica");
+        });
+        test("should return email when getEmail is called", () => {
+            const intern = new Intern(mockIntern);
+            expect(intern.getEmail()).toEqual("jessica@test.com");
+        });
+        test("should return school when getSchool is called", () => {
+            const intern = new Intern(mockIntern);
+            expect(intern.getGithub()).toEqual("Columbia University");
+        });
+        test("should return intern role when getRole is called", () => {
+            const intern = new Intern(mockIntern);
+            expect(intern.getRole()).toEqual("Intern");
+        });
+    });
+});
